172377da5affb649ed52c4f3a4185814d9d3238d
David Whaley <davidwhaleycode@outlook.com>
2018-10-27 12:47:41 -0500

    'contact-ui'

--- BoardSystems/BoardSystems/Global.asax.cs
+++ BoardSystems/BoardSystems/Global.asax.cs
@@ -6,12 +6,14 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
+
 namespace BoardSystems
 {
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
-        {
+        {        
+            
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
