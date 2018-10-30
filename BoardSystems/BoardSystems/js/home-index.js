44e0ffb110e2c1c63330db4b3c6d20bed86d2c26
David Whaley <davidwhaleycode@outlook.com>
2018-10-30 11:23:00 -0500

    moreangular

--- BoardSystems/BoardSystems/js/home-index.js
+++ BoardSystems/BoardSystems/js/home-index.js
@@ -1,17 +1,22 @@
 ﻿// home-index.js
 
 function homeIndexController($scope, $http) {
-    $scope.dataCount = 0;
+    
     $scope.data = [];
+    $scope.isBusy = true;
 
     $http.get("/api/v1/topics?includeReplies=true")
         .then(function (result) {
             //successful
             angular.copy(result.data, $scope.data);
+            
         },
-        function () {
-            //Error
-            alert("could not load topics");
-        })
+            function () {
+                //Error
+                alert("could not load topics");
+            })
+        .then(function () {
+            $scope.isBusy = false;
+        });
 
 }
